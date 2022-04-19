37978d809a21cf97fb3ba4dfbf5da3f818fb7b02
Hyunu Jung <hyunujung0419@gmail.com>
2022-04-19 17:25:52 +0900

    commands/*.js indent, rm test.js

--- commands/help.js
+++ commands/help.js
@@ -2,17 +2,17 @@ let { MessageEmbed } = require('discord.js');
 const img = 'https://blog.kakaocdn.net/dn/qpua2/btqyqx0g6YA/NYd5fopPNOBPwxDiYIXDK1/img.jpg';
 
 module.exports = {
-    name: "도움말",
-    description: "도움말을 표시합니다.",
-    execute(msg) {
-        const helpMessage = new MessageEmbed()
-            .setAuthor('검열봇', img)
-            .setTitle('**📜 도움말**')
-            .setColor(0xBDBDBD)
-            .addFields(
-                {name: '경고 증가, 차감', value: '욕설 사용 시 경고가 증가되고, 좋은 단어 사용 시 경고가 차감됩니다.'},
-                {name: '경고수', value: 'ㅁ경고수 -> 본인의 경고수\nㅁ경고수 @유저 -> 유저의 경고수\n를 표시합니다.'}
-            );
-        msg.channel.send({ embeds: [helpMessage] });
-    }
+  name: "도움말",
+  description: "도움말을 표시합니다.",
+  execute(msg) {
+    const helpMessage = new MessageEmbed()
+      .setAuthor('검열봇', img)
+      .setTitle('**📜 도움말**')
+      .setColor(0xBDBDBD)
+      .addFields(
+        { name: '경고 증가, 차감', value: '욕설 사용 시 경고가 증가되고, 좋은 단어 사용 시 경고가 차감됩니다.' },
+        { name: '경고수', value: 'ㅁ경고수 -> 본인의 경고수\nㅁ경고수 @유저 -> 유저의 경고수\n를 표시합니다.' }
+      );
+    msg.channel.send({ embeds: [helpMessage] });
+  }
 }
--- commands/test.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    "name": "테스트",
-    "description": "테스트 명령어",
-    execute(msg) {
-        msg.reply('Hello, World!');
-    }
-}
