ea5d9d8e711e738f79b6903b45a51a9d67b84e9e
Hyunu Jung <hyunujung0419@gmail.com>
2022-04-20 21:22:03 +0900

    not good user filter

--- commands/warnadd.js
+++ commands/warnadd.js
@@ -8,15 +8,23 @@ module.exports = {
   execute(msg) {
     const user = msg.mentions.users.first();
     const temp = msg.content.slice(4);
-    const target = user.id;
     const guild = msg.guild.id;
     const warn_num = Number(msg.content.slice(26));
+
+    if (!temp.startsWith('<@') && !temp.endsWith('>') || temp.startsWith('<@&')) {
+      const answerMessage = new MessageEmbed()
+        .setAuthor('검열봇', img)
+        .setTitle('**경고 초기화**')
+        .setDescription(`${temp} (이)라는 유저는 존재하지 않습니다!`);
+      msg.reply({ embeds: [answerMessage] });
+      return;
+    }
+
+    const target = user.id;
     const warn_get = `warn.${guild}.${target}`;
     const warn = db.get(warn_get);
 
-    db.add(warn_get, warn_num);
-
-    if (warn == NaN) {
+    if (user == NaN) {
       const answerMessage = new MessageEmbed()
         .setAuthor('검열봇', img)
         .setTitle('**경고 추가**')
@@ -30,7 +38,9 @@ module.exports = {
       .setTitle('**경고 추가**')
       .setColor(0xBDBDBD)
       .setDescription(`<@${target}> 님의 경고를 ${warn_num} 만큼 추가합니다.`)
-      .addField('누적 경고수', `${warn + warn_num} -> ${warn}`);
+      .addField('누적 경고수', `${warn == NaN || !warn ? 0 : warn - warn_num + 1} -> ${warn == NaN ? warn_num : warn + warn_num}`);
+    if (warn == NaN) db.set(warn_get, warn_num);
+    else db.add(warn_get, warn_num);
     msg.reply({ embeds: [answerMessage] });
   }
 }
--- commands/warnremove.js
+++ commands/warnremove.js
@@ -7,26 +7,37 @@ module.exports = {
   description: "경고를 차감합니다.",
   execute(msg) {
     const user = msg.mentions.users.first();
-    const target = user.id;
     const guild = msg.guild.id;
+    const temp = msg.content.slice(6);
     let warn_num = 0 - Number(msg.content.slice(28));
-    const warn_get = `warn.${guild}.${target}`;
-    const warn = db.get(warn_get);
     let flag = false;
 
-    if (warn + warn_num < 0) {
+    if (!temp.startsWith('<@') && !temp.endsWith('>') || temp.startsWith('<@&')) {
+      const answerMessage = new MessageEmbed()
+        .setAuthor('검열봇', img)
+        .setTitle('**경고 초기화**')
+        .setDescription(`${temp} (이)라는 유저는 존재하지 않습니다!`);
+      msg.reply({ embeds: [answerMessage] });
+      return;
+    }
+
+    const target = user.id;
+    const warn_get = `warn.${guild}.${target}`;
+    const warn = db.get(warn_get);
+
+    if (warn + warn_num < 0 || warn == NaN) {
       warn_num = 0 - warn;
       flag = true;
     }
-
-    db.add(warn_get, warn_num);
 
     const answerMessage = new MessageEmbed()
       .setAuthor('검열봇', img)
       .setTitle('**경고 차감**')
       .setColor(0xBDBDBD)
       .setDescription(`<@${target}> 님의 경고를 ${0 - warn_num} 만큼 차감합니다.\n${flag ? '경고 차감 횟수가 기존 경고보다 많아 0이 되었습니다!' : '\n'}`)
-      .addField('누적 경고수', `${warn - warn_num} -> ${warn}`);
+      .addField('누적 경고수', `${warn == NaN || !warn ? 0 : warn - warn_num} -> ${warn == NaN || !warn ? 0 : warn}`);
+    if (warn == NaN) db.set(warn_get, warn_num);
+    else db.add(warn_get, warn_num);
     msg.reply({ embeds: [answerMessage] });
   }
 }
