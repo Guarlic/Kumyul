87f7c26075fdacbfbb6ee43116e32dbfc468f9ac
Hyunu Jung <hyunujung0419@gmail.com>
2022-04-22 21:36:14 +0900

    밴 -> 킥

--- commands/kick.js
+++ commands/kick.js
@@ -1,5 +1,4 @@
 const db = require('quick.db');
-const img = 'https://cdn.discordapp.com/attachments/938745566647705690/966469502692900874/ab9ac7ad6be1ac73.jpeg';
 
 module.exports = {
   name: "킥",
@@ -19,7 +18,7 @@ module.exports = {
     db.set(warn_get, 0);
     msg.reply('대상을 킥합니다.');
     msg.guild.members.kick(id)
-      .then(banInfo => console.log(`${banInfo.user?.tag ?? banInfo.tag ?? banInfo} 를 밴했습니다.`))
+      .then(banInfo => console.log(`${banInfo.user?.tag ?? banInfo.tag ?? banInfo} 를 킥했습니다.`))
       .catch(console.error);
   }
 }
